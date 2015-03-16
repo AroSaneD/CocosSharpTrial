dc98e5e68791bc03bc88d4ec84b4fcfc3e4532a7
AroSaneD <mail.junk.3135@gmail.com>
2015-03-16 19:59:04 +0200

    Added custome game scene class

---
+++ Cocos2DGame1/CustomGameScene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cocos2D;
+
+namespace Cocos2DGame1
+{
+    class CustomGameScene : CCScene
+    {
+
+    }
+}
