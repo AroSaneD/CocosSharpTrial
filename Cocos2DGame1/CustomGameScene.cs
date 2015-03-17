0ed5240491b8bcd095affab2cb4645d31a13f8c1
AroSaneD <mail.junk.3135@gmail.com>
2015-03-17 12:28:48 +0200

    added ball

--- Cocos2DGame1/CustomGameScene.cs
+++ Cocos2DGame1/CustomGameScene.cs
@@ -13,12 +13,39 @@ namespace Cocos2DGame1
 			CCLayerColor layer = new CCLayerColor();
 			AddChild(layer);
 
-			CCSprite sprite = new CCSprite(new CCRect(0, 0, 50, 50));
-			sprite.Color = CCColor3B.Green;
+			#region creating the platform
+			CCSprite sprite = new CCSprite(new CCRect(0, 0, 100, 20));
+			sprite.Color = CCColor3B.Blue;
 
 			layer.AddChild(sprite);
-			sprite.Position = CCDirector.SharedDirector.WinSize.Center;
 
+			CCPoint lowerCenter = new CCPoint();
+
+			lowerCenter.X = CCDirector.SharedDirector.WinSize.Center.X;
+			lowerCenter.Y = (int)((float)CCDirector.SharedDirector.WinSize.Center.Y * 0.2);
+
+			sprite.Position = lowerCenter;
+
+			#endregion
+
+			
+			#region creating the ball
+
+			CCSprite ball = new CCSprite(new CCRect(0, 0, 15, 15));
+			ball.Color = CCColor3B.Green;
+			
+			layer.AddChild(ball);
+			
+			CCPoint upperRegion = new CCPoint();
+			
+			upperRegion.Y = (int)((float)CCDirector.SharedDirector.WinSize.Center.Y * 1.5);
+			upperRegion.X = (int)((float) CCDirector.SharedDirector.WinSize.Center.X * 1.3);
+			
+			ball.Position = upperRegion;
+			
+			#endregion
+			
+			
 			layer.Color = CCColor3B.Black;
 			layer.Opacity = 255;
 		}
